9e12ce54641603590ee545ed8ebf74f9b0636892
Matilda Sutherland <girlonroad@192-168-1-163.tpgi.com.au>
2024-12-10 09:52:06 +1100

    Link to wrappers manually

--- src/components/Artists/ArtistPage.jsx
+++ src/components/Artists/ArtistPage.jsx
@@ -1,5 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import { useParams } from "react-router-dom";
+import RoyAnandaWrapper from "./RoyAnandaWrapper";
+import BrieTrenerryWrapper from "./BrieTrenerryWrapper";
+import HiballWrapper from "./HiballWrapper";
+import GirlOnRoadWrapper from "./GirlOnRoadWrapper";
+import JonRafmanWrapper from "./JonRafmanWrapper";
+import "./ArtistPage.css"; // Import CSS for fade-in effect
 
 const generateSlug = (title) => {
   return title
@@ -17,30 +23,40 @@ const convertSlugToName = (slug) => {
 
 const ArtistPage = () => {
   const { artistSlug, artworkSlug } = useParams();
-  const [content, setContent] = useState(null);
+  const [Component, setComponent] = useState(null);
 
   useEffect(() => {
-    const loadComponent = async () => {
-      const artistName = convertSlugToName(artistSlug).replace(/\s+/g, "");
-      console.log("Loading component for artist:", artistName); // Debug statement
-      let component;
-      try {
-        component = await import(`./${artistName}Wrapper`);
-      } catch (error) {
-        console.error("Error loading component:", error);
-        component = null;
-      }
-      if (component && component.default) {
-        setContent(() => component.default);
-      }
-    };
-    loadComponent();
+    const artistName = convertSlugToName(artistSlug).replace(/\s+/g, "");
+    console.log("Loading component for artist:", artistName); // Debug statement
+
+    switch (artistName) {
+      case "RoyAnanda":
+        setComponent(() => RoyAnandaWrapper);
+        break;
+      case "BrieTrenerry":
+        setComponent(() => BrieTrenerryWrapper);
+        break;
+      case "Hiball":
+        setComponent(() => HiballWrapper);
+        break;
+      case "GirlOnRoad":
+        setComponent(() => GirlOnRoadWrapper);
+        break;
+      case "JonRafman":
+        setComponent(() => JonRafmanWrapper);
+        break;
+      default:
+        console.error("Artist component not found:", artistName);
+        setComponent(null);
+    }
   }, [artistSlug, artworkSlug]);
 
   return (
-    <div>
-      {content ? (
-        React.createElement(content, { artworkSlug })
+    <div className="artist-page">
+      {Component ? (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Component artworkSlug={artworkSlug} />
+        </Suspense>
       ) : (
         <p>Loading...</p>
       )}
